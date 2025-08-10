8abdf302e927ce61e2da2de4d08204880e271b18
Nilson Brites <nilson.brites@example.com>
2025-08-10 05:39:54 -0400

    Revert "feat: Implementa modais de login e cadastro, corrige redirecionamento e atualiza logo"
    This reverts commit d6271967cbf2abde7bce989b307501ddf73daf24.

--- sistema.js
+++ sistema.js
@@ -385,12 +385,14 @@ function realizarCadastro() {
   
   // Salvar dados
   if (salvarDados()) {
-    mostrarMensagem(\'msgCadastro\', \'Cadastro realizado com sucesso!\', \'success\');
-    debug(\'Usuário cadastrado com sucesso\', usuarios[cpf]);
-    closeModal(\'cadastroModal\');
-    openModal(\'loginModal\');
+    mostrarMensagem('msgCadastro', 'Cadastro realizado com sucesso! Redirecionando...', 'success');
+    debug('Usuário cadastrado com sucesso', usuarios[cpf]);
+    
+    setTimeout(function() {
+      window.location.href = 'dashboard.html';
+    }, 2000);
   } else {
-    mostrarMensagem(\'msgCadastro\', \'Erro ao salvar dados. Tente novamente.\', \'error\');
+    mostrarMensagem('msgCadastro', 'Erro ao salvar dados. Tente novamente.', 'error');
   }
 }
 
