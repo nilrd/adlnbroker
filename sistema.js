5a7bda06fc27657553ad64e22b6d2ad842488ed5
Nilson Brites <nilson.brites@gmail.com>
2025-09-06 21:34:37 -0300

    correções

--- sistema.js
+++ sistema.js
@@ -61,13 +61,14 @@ var precos = {
   ABEV3: 14.25,
   MGLU3: 3.45,
   BBAS3: 49.10,
-  LREN3: 18.30
+  LREN3: 18.30,
+  COGN3: 16.00
 };
 
 // Expor preços globalmente para sincronização com new-chart.js
 window.precos = precos;
 
-var ativos = ['PETR4', 'VALE3', 'ITUB4', 'BBDC4', 'ABEV3', 'MGLU3', 'BBAS3', 'LREN3'];
+var ativos = ['PETR4', 'VALE3', 'ITUB4', 'BBDC4', 'ABEV3', 'MGLU3', 'BBAS3', 'LREN3', 'COGN3'];
 
 // Função para debug
 function debug(msg, data) {
@@ -1990,10 +1991,6 @@ function atualizarModalCarteira() {
           'MGLU3': 7.50,
           'BBAS3': 35.00,
           'LREN3': 20.00,
-          'WEGE3': 40.00,
-          'B3SA3': 10.00,
-          'COGN3': 16.00,
-          'ITSA4': 8.50
         };
         precoBase = precosReferencia[ativo] || precoAtual;
       }
@@ -2090,10 +2087,7 @@ function atualizarModalCarteira() {
             'MGLU3': 7.50,
             'BBAS3': 35.00,
             'LREN3': 20.00,
-            'WEGE3': 40.00,
-            'B3SA3': 10.00,
-            'COGN3': 16.00,
-            'ITSA4': 8.50
+            'COGN3': 16.00
           };
           precoBase = precosReferencia[ativo] || precoAtual;
         }
